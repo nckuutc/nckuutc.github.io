971791507306e5a4f5a7eeaa675da8c2181fae9a
nckuutc <utcncku.gmail.com>
2016-12-11 15:10:45 +0800

    Updare v1.0

--- js/index.js
+++ js/index.js
@@ -1,3 +1,50 @@
+// Typr animation
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+
+};
+
+
 // Lock zoom
 var zoomlevel = 1;
 
@@ -28,14 +75,16 @@ $(window).load(function() {
 
     function resizeBg() {
         if ((theWindow.width() / theWindow.height()) < aspectRatio) {
-            $bg
-                .removeClass()
+            $bg.removeClass()
                 .addClass('bgheight');
         } else {
-            $bg
-                .removeClass()
+            $bg.removeClass()
                 .addClass('bgwidth');
         }
+
+        $('.slideshow').cycle({
+            timeout: 9000
+        });
     }
 
     theWindow.resize(resizeBg).trigger('resize');
